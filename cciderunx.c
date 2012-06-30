2b06fc16b8969dc4b63838bc5b91ab1cec28476b
Tom Young <githb@twyoung.com>
2012-06-30 11:13:37 -0400

    Update to GPL-v3

--- cciderunx.c
+++ cciderunx.c
@@ -1,20 +1,26 @@
+/*  	ccide - C Language Decision Table Code Generator 
+	Copyright (C) 2002-2004,2010,2012;  Thomas W. Young, e-mail:  ccide@twyoung.com
+
+    	This file is part of ccide, the C Language Decision Table Code Generator.
+
+   	Ccide is free software: you can redistribute it and/or modify
+   	it under the terms of the GNU General Public License as published by
+    	the Free Software Foundation, either version 3 of the License, or
+   	(at your option) any later version.
+
+    	Ccide is distributed in the hope that it will be useful,
+    	but WITHOUT ANY WARRANTY; without even the implied warranty of
+    	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    	GNU General Public License for more details.
+
+    	You should have received a copy of the GNU General Public License
+    	along with Ccide.  If not, see <http://www.gnu.org/licenses/> or
+    	write to the Free Software Foundation, Inc., 51 Franklin St, 
+    	Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 /* cciderun.c  run time decision tables                                      */
 
-/*         ccide - C Language Decision Table Code Generator                  */
-/* Copyright (C) 2002-2010 Thomas W. Young, e-mail:  ccide@twyoung.com       */
-/* This program is free software; you can redistribute it and/or modify      */
-/* it under the terms of the GNU General Public License as published by      */
-/* the Free Software Foundation.                                             */
-/*                                                                           */
-/* This program is distributed in the hope that it will be useful,           */
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
-/* GNU General Public License for more details.                              */
-/*                                                                           */
-/* You should have received a copy of the GNU General Public License         */
-/* along with this program; if not, write to the Free Software               */
-/* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 */
-/* ----------------------------------------------- end of legal stuff ------ */
  
 #include "ccide.h"
 #include <stdlib.h>
