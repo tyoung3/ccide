75d96e54e3517de5fd07d37f0b32dda55b883607
Tom Young <githb@twyoung.com>
2012-08-22 11:27:40 -0400

    Added VB to tests

---
+++ tests/ex4.vb.d
@@ -0,0 +1,16 @@
+' Taken from http://www.romankoch.ch/capslock/vbdectable.htm
+
+'CCIDE_INLINE_CODE:
+
+A$ = INPUT$
+
+'DECISION_TABLE:
+'  1 2 3 4 | A$ == "$$"
+'  _______ | __________
+'  X - - - | PRINT 1
+'  - X - - | PRINT 2
+'  - - X - | PRINT 3
+'  - - - X | PRINT 4
+'END_TABLE:
+
+
